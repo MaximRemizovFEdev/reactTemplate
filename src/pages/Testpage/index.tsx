8e7a8fb1928ffabf11c32b2377e37caaea261644
remizov.ms <remizov.ms@gdcloud.org>
2024-09-10 17:53:35 +0300

    feat: add footer component

--- src/pages/Testpage/index.tsx
+++ src/pages/Testpage/index.tsx
@@ -1,7 +1,19 @@
+import { Footer } from "../../components/Footer";
+
 export function Testpage() {
 	return (
 		<div class="home">
 			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!</h2>
+            <Footer />
 		</div>
 	);
 }
