21025c57937f4ab65584c09e9d333a3fec491567
remizov.ms <remizov.ms@gdcloud.org>
2024-09-10 18:52:48 +0300

    feat: add label component

--- src/pages/Testpage/index.tsx
+++ src/pages/Testpage/index.tsx
@@ -1,14 +1,15 @@
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header/Header";
+import { Label } from "../../components/Label";
 
 export function Testpage() {
 	return (
 		<div class="home">
 			<Header />
-			<h2>Hello world!!!</h2>
-			<h2>Hello world!!!</h2>
-			<h2>Hello world!!!</h2>
-			<h2>Hello world!!!</h2>
+			<h2>Hello world!!!<Label text={"Подарок"} color={"blue"}/><Label text={"Игрушка"} color={"pink"}/></h2>
+			<h2>Hello world!!!<Label text={"Мартышка"} color={"green"}/></h2>
+			<h2>Hello world!!!<Label text={"Подарок"} color={"orange"}/></h2>
+			<h2>Hello world!!!<Label text={"Подарок"} color={"pink"}/></h2>
 			<h2>Hello world!!!</h2>
 			<h2>Hello world!!!</h2>
 			<h2>Hello world!!!</h2>
